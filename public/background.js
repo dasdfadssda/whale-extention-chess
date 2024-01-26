5bce60e2b64738128d6c82b4871f7d9ee1623882
dasdfadssda <quddkflty@gmail.com>
2024-01-26 23:35:33 +0900

    사이드바 설정

---
+++ public/background.js
@@ -0,0 +1,14 @@
+// background.js
+
+// 웨일 브라우저 API를 사용하여 익스텐션의 기능을 구현합니다.
+// 예: 사이드바를 열 때 메시지를 출력합니다.
+whale.sidebarAction.onClicked.addListener(() => {
+    console.log('Sidebar opened');
+  });
+  
+  // 웨일 브라우저 API를 사용하여 탭, 알림, 스토리지 등을 관리합니다.
+  // 예: 새 탭을 열 때 메시지를 출력합니다.
+  whale.tabs.onCreated.addListener((tab) => {
+    console.log('New tab opened: ' + tab.id);
+  });
+  
--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import styled from "styled-components";
 
 function App() {
   return (
-    <div >
-      시작화면
-    </div>
+    <Div>
+      <h1>우하하하 해냈다</h1>
+    </Div>
   );
 }
 
 export default App;
+
+const Div = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
