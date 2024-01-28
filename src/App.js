afd3e686a1ae6ff8fffbfff4f261b351d64f12e2
dasdfadssda <quddkflty@gmail.com>
2024-01-29 00:18:29 +0900

    set route with pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LanguageProvider } from "./Context/LanguageContext";
 import HomePage from "./Pages/HomePage";
 import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
+    <LanguageProvider>
     <Router>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
       </Router>
+      </LanguageProvider>
   );
 }
 
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,16 +1,13 @@
 import styled from "styled-components";
-import { LanguageProvider } from "../Context/LanguageContext";
 import LanguageComponent from "../Components/LanguageComponent";
 import LanguageSelector from "../Components/LanguageSelector";
 
 function HomePage() {
   return (
     <Div>
-      <LanguageProvider>
         <h1>언어를 선택해주세요.</h1>
         <LanguageSelector />
         <LanguageComponent />
-      </LanguageProvider>
     </Div>
   );
 }
