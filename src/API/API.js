e4898fd70f397a2ad2d2939ccafc0eaadf141c9a
KimRPG <hunsung9909@gmail.com>
2024-02-05 10:56:55 +0900

    API 오류

--- src/API/API.js
+++ src/API/API.js
@@ -1,49 +1,55 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react'; // useContext 제거
+import { DifficultyContext } from '../Context/DifficultyContext'; 
 
-// function boardToFen(board) {
-//     const pieceToFenMap = {
-//       "pawn": "p",
-//       "knight": "n",
-//       "bishop": "b",
-//       "rook": "r",
-//       "queen": "q",
-//       "king": "k",
-//       null: "1"
-//     };
+function boardToFen(board) {
+    const pieceToFenMap = {
+      "pawn": "p",
+      "knight": "n",
+      "bishop": "b",
+      "rook": "r",
+      "queen": "q",
+      "king": "k",
+      null: "1"
+    };
   
-//     let fenString = '';
+    let fenString = '';
   
-//     for (let row of board) {
-//       let fenRow = '';
-//       let emptySquareCount = 0;
+    for (let row of board) {
+      let fenRow = '';
+      let emptySquareCount = 0;
   
-//       for (let square of row) {
-//         if (square !== null) {
-//           if (emptySquareCount > 0) {
-//             fenRow += emptySquareCount;
-//             emptySquareCount = 0;
-//           }
-//           const fenPiece = pieceToFenMap[square.type];
-//           fenRow += square.color === "black" ? fenPiece : fenPiece.toUpperCase();
-//         } else {
-//           emptySquareCount++;
-//           if (emptySquareCount === 8) {
-//             fenRow += '8';
-//             emptySquareCount = 0;
-//           }
-//         }
-//       }
-//       if (fenRow.length !== 8 && fenRow !== "8") {
-//         fenRow += emptySquareCount;
-//       }
-//       fenString += fenRow + '/';
-//     }
+      for (let square of row) {
+        if (square !== null) {
+          if (emptySquareCount > 0) {
+            fenRow += emptySquareCount;
+            emptySquareCount = 0;
+          }
+          const fenPiece = pieceToFenMap[square.type];
+          fenRow += square.color === "black" ? fenPiece : fenPiece.toUpperCase();
+        } else {
+          emptySquareCount++;
+          if (emptySquareCount === 8) {
+            fenRow += '8';
+            emptySquareCount = 0;
+          }
+        }
+      }
+      if (fenRow.length !== 8 && fenRow !== "8") {
+        fenRow += emptySquareCount;
+      }
+      fenString += fenRow + '/';
+    }
   
-//     fenString = fenString.slice(0, -1);
+    fenString = fenString.slice(0, -1);
   
-//     return fenString;
-// }
+    return fenString;
+}
+
+function fenAPI(board, computer, turn) {
+    const fen = boardToFen(board) + " " + computer + " - - 5 " + turn;
+    return fen; // 수정: 문자열을 반환하도록 수정
+}
 
 function bestMove({ board }) {
   const [bestMove, setBestMove] = useState();
@@ -81,4 +87,3 @@ function bestMove({ board }) {
 
   return bestMove;
 }
-export default bestMove;
