a7898f5f960bae4e530f611c2e88b7b5080fe492
dasdfadssda <quddkflty@gmail.com>
2024-02-08 16:05:10 +0900

    SettingPage route 설정

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ROUTES from "./Static/Constants/route";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./Styles/Theme";
 import GlobalStyle from "./Styles/GlobalStyle";
+import ChessSettings from "./Pages/SettingPage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Routes>
             <Route path={ROUTES.HOME} element={<HomePage />} />
             <Route path={ROUTES.CHESS} element={<ChessBoard />} />
+            <Route path={ROUTES.SETTING} element={<ChessSettings />} />
           </Routes>
         </Switch>
       </DifficultyProvider>
