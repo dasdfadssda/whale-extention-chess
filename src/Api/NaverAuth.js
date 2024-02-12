152bfe801ba6120384169f5e4b6c39da5c5daabe
dasdfadssda <quddkflty@gmail.com>
2024-02-12 15:49:16 +0900

    fix: error

--- src/Api/NaverAuth.js
+++ src/Api/NaverAuth.js
@@ -42,7 +42,7 @@ export const getUserInfo = async (accessToken) => {
     saveUserToFirebase(userToSave, userInfo.id);
 
     return userInfo;
-  } catch (error) {s
+  } catch (error) {
     console.error("사용자 정보를 가져오는 데 실패했습니다.", error);
   }
 };
