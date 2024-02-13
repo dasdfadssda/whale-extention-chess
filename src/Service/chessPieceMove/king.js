eb3a5784644aa9042d487f75a477f15f8d20f269
dasdfadssda <quddkflty@gmail.com>
2024-02-13 19:13:39 +0900

    캐슬링 권한 부여

--- src/Service/chessPieceMove/king.js
+++ src/Service/chessPieceMove/king.js
@@ -1,22 +1,31 @@
-export const getKingMoves = (x, y, board, color) => {
-    let moves = [    [x - 1, y - 1],
-      [x - 1, y],
-      [x - 1, y + 1],
-      [x, y - 1],
-      [x, y + 1],
-      [x + 1, y - 1],
-      [x + 1, y],
-      [x + 1, y + 1],
-    ];
-  
-    return moves.filter(([x, y]) => {
-      return (
-        x >= 0 &&
-        x < 8 &&
-        y >= 0 &&
-        y < 8 &&
-        (!board[x][y] || board[x][y].color !== color)
-      );
-    });
-  };
-  
+export const getKingMoves = (x, y, board, color, castlingRights) => {
+  let moves = [
+    [x - 1, y - 1],
+    [x - 1, y],
+    [x - 1, y + 1],
+    [x, y - 1],
+    [x, y + 1],
+    [x + 1, y - 1],
+    [x + 1, y],
+    [x + 1, y + 1],
+  ];
+
+  // 캐슬링 가능한 경우 추가
+  if (color === "white") {
+    if (castlingRights.whiteKingSide) moves.push([7, 6]);
+    if (castlingRights.whiteQueenSide) moves.push([7, 2]);
+  } else {
+    if (castlingRights.blackKingSide) moves.push([0, 6]);
+    if (castlingRights.blackQueenSide) moves.push([0, 2]);
+  }
+
+  return moves.filter(([x, y]) => {
+    return (
+      x >= 0 &&
+      x < 8 &&
+      y >= 0 &&
+      y < 8 &&
+      (!board[x][y] || board[x][y].color !== color)
+    );
+  });
+};
--- src/Service/chessPieceMove/rook.js
+++ src/Service/chessPieceMove/rook.js
@@ -1,25 +1,22 @@
 export const getRookMoves = (x, y, board, color) => {
-    let moves = [];
-    let directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-  
-    for (let [dx, dy] of directions) {
-      for (let i = 1; i < 8; i++) {
-        let nx = x + dx * i;
-        let ny = y + dy * i;
-  
-        if (nx < 0 || nx >= 8 || ny < 0 || ny >= 8)
-          break;
-  
-        if (board[nx][ny]) {
-          if (board[nx][ny].color !== color)
-            moves.push([nx, ny]);
-          break;
-        }
-  
-        moves.push([nx, ny]);
+  let moves = [];
+  let directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+  for (let [dx, dy] of directions) {
+    for (let i = 1; i < 8; i++) {
+      let nx = x + dx * i;
+      let ny = y + dy * i;
+
+      if (nx < 0 || nx >= 8 || ny < 0 || ny >= 8) break;
+
+      if (board[nx][ny]) {
+        if (board[nx][ny].color !== color) moves.push([nx, ny]);
+        break;
       }
+
+      moves.push([nx, ny]);
     }
-  
-    return moves;
-  };
-  
+  }
+
+  return moves;
+};
