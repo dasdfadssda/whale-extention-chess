c89035a67077a4644022d9dee1e29ce3712620c6
dasdfadssda <quddkflty@gmail.com>
2024-02-14 22:05:33 +0900

    캐싱 로직 추가

--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,33 +1,46 @@
 // UserContext.js
-import React, { createContext, useContext, useEffect } from "react";
-import { UserModel } from '../Model/UserModel'; 
-import { fetchUserFromFirebase } from '../Service/Auth/GetUserData';
-
+import React, { createContext, useContext } from "react";
+import { UserModel } from "../Model/UserModel";
+import { fetchUserFromFirebase } from "../Service/Auth/GetUserData";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   // 전역 변수 API 선언
   const [user, setUser] = React.useState(
-    () => window.sessionStorage.getItem("userData") || UserModel);
-  
+    () => window.sessionStorage.getItem("userData") || UserModel
+  );
+
   React.useEffect(() => {
     window.sessionStorage.setItem("userData", user);
   }, [user]);
-  
+
+  // 로컬 스토리지에 마지막으로 사용자 데이터가 업데이트된 시간을 저장
+  const [lastUpdatedTime, setLastUpdatedTime] = React.useState(
+    () => window.localStorage.getItem("lastUpdatedTime") || 0
+  );
+
+  // 사용자 데이터가 업데이트된 시간을 로컬 스토리지에 저장
+  React.useEffect(() => {
+    window.sessionStorage.setItem("userData", JSON.stringify(user));
+    setLastUpdatedTime(Date.now()); // 현재 시간을 밀리초로 저장
+  }, [user]);
 
   // 유저 관리
-  useEffect(() => {
+  React.useEffect(() => {
     const fetchUserData = async () => {
       const userId = localStorage.getItem("id");
 
-      const userData = await fetchUserFromFirebase(userId);
-      if (userData) {
-        setUser(userData);
+      // 최근 30초 이내에 업데이트된 경우에만 사용자 데이터를 가져옴
+      if (Date.now() - lastUpdatedTime <= 30000) {
+        const userData = await fetchUserFromFirebase(userId);
+        if (userData) {
+          setUser(userData);
+        }
       }
     };
     fetchUserData();
-  }, []);
+  }, [lastUpdatedTime]);
 
   console.log("user 정보 :", user);
 
