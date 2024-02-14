d589d0437e4253b7ff2fc627558f6dbf4d1c1cfe
dasdfadssda <quddkflty@gmail.com>
2024-02-14 18:04:28 +0900

    테스트 콘솔 추가

--- src/Context/DifficultyContext.js
+++ src/Context/DifficultyContext.js
@@ -4,7 +4,7 @@ export const DifficultyContext = React.createContext();
 
 export function DifficultyProvider({ children }) {
   const [difficulty, setDifficulty] = React.useState("");
-
+  console.log("선택한 난이도: ",difficulty)
   return (
     <DifficultyContext.Provider value={{ difficulty, setDifficulty }}>
       {children}
