3edb098f632d74a944abc9b7c9ad95d68aab5826
dasdfadssda <quddkflty@gmail.com>
2024-02-14 22:00:42 +0900

    세션 스토리지 추가

--- src/Context/DifficultyContext.js
+++ src/Context/DifficultyContext.js
@@ -3,8 +3,16 @@ import React from "react";
 export const DifficultyContext = React.createContext();
 
 export function DifficultyProvider({ children }) {
-  const [difficulty, setDifficulty] = React.useState("");
-  console.log("선택한 난이도: ",difficulty)
+  const [difficulty, setDifficulty] = React.useState(
+    () => window.sessionStorage.getItem("difficulty") || ""
+  );
+  
+  React.useEffect(() => {
+    window.sessionStorage.setItem("difficulty", difficulty);
+  }, [difficulty]);
+  
+  console.log("선택한 난이도: ", difficulty);
+  
   return (
     <DifficultyContext.Provider value={{ difficulty, setDifficulty }}>
       {children}
--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -3,24 +3,31 @@ import React, { createContext, useContext, useEffect } from "react";
 import { UserModel } from '../Model/UserModel'; 
 import { fetchUserFromFirebase } from '../Service/Auth/GetUserData';
 
+
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = React.useState(UserModel);
+  // 전역 변수 API 선언
+  const [user, setUser] = React.useState(
+    () => window.sessionStorage.getItem("userData") || UserModel);
+  
+  React.useEffect(() => {
+    window.sessionStorage.setItem("userData", user);
+  }, [user]);
+  
 
+  // 유저 관리
   useEffect(() => {
     const fetchUserData = async () => {
       const userId = localStorage.getItem("id");
 
-    
-     const userData = await fetchUserFromFirebase(userId);
+      const userData = await fetchUserFromFirebase(userId);
       if (userData) {
         setUser(userData);
       }
     };
     fetchUserData();
   }, []);
-  
 
   console.log("user 정보 :", user);
 
