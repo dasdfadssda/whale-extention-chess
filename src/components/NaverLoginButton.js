76e7e9b9ef1435815646f067ba0a6264e19d49a0
dasdfadssda <quddkflty@gmail.com>
2024-02-18 16:04:54 +0900

    UI 수정

--- src/components/NaverLoginButton.js
+++ src/components/NaverLoginButton.js
@@ -1,5 +1,6 @@
 // NaverLoginButton.js
 import React from "react";
+import styled from "styled-components";
 
 // 상태 토큰 생성 함수
 function generateState() {
@@ -12,7 +13,6 @@ function NaverLoginButton() {
   // const NAVER_CALLBACK_URL = "https://chessextension.web.app/#/callback";
   // const NAVER_CALLBACK_URL = "http://localhost:3000/#/callback";
 
-
   const handleNaverLogin = () => {
     const state = generateState();
     const NAVER_LOGIN_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${encodeURIComponent(
@@ -22,7 +22,28 @@ function NaverLoginButton() {
     window.location.href = NAVER_LOGIN_URL;
   };
 
-  return <button onClick={handleNaverLogin}>네이버로 로그인하기</button>;
+  return (
+    <NaverButton onClick={handleNaverLogin}>
+      <img
+        src={require("../Static/Assets/NaverLoginButton.png")}
+        alt="네이버 로그인 버튼"
+      />
+    </NaverButton>
+  );
 }
 
 export default NaverLoginButton;
+
+const NaverButton = styled.button`
+  width: 36vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  background-color: transparent;
+  margin-top: 4vw;
+  img {
+    max-width: 85%;
+    max-height: 85%;
+  }
+`;
