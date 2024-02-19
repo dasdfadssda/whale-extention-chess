9575cab292c946982c29368db2e9b77305852b36
dasdfadssda <quddkflty@gmail.com>
2024-02-19 18:39:50 +0900

    리펙토링

--- src/Api/NaverAuth.js
+++ src/Api/NaverAuth.js
@@ -5,12 +5,15 @@ import { saveUserToFirebase } from "../Service/Auth/SetUserData";
 // 유저 토큰 발급
 export const getAccessToken = async (naverCode, savedState) => {
   try {
-    const response = await axios.get(`${process.env.REACT_APP_URL}/naver/token/`, {
-      params: {
-        code: naverCode,
-        state: savedState,
-      },
-    });
+    const response = await axios.get(
+      `${process.env.REACT_APP_URL}/naver/token/`,
+      {
+        params: {
+          code: naverCode,
+          state: savedState,
+        },
+      }
+    );
     return response.data.access_token;
   } catch (error) {
     console.error("액세스 토큰을 가져오는 데 실패했습니다.", error);
@@ -21,11 +24,14 @@ export const getAccessToken = async (naverCode, savedState) => {
 // 유저 정보 읽기
 export const getUserInfo = async (accessToken, setUser) => {
   try {
-    const response = await axios.get(`${process.env.REACT_APP_URL}/naver/userinfo/`, {
-      params: {
-        access_token: accessToken,
-      },
-    });
+    const response = await axios.get(
+      `${process.env.REACT_APP_URL}/naver/userinfo/`,
+      {
+        params: {
+          access_token: accessToken,
+        },
+      }
+    );
     const userInfo = response.data.user_info;
 
     // UserModel에 사용자 정보를 채워넣기
