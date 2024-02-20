0793223336743e69c8826ed225914ede35c2d4aa
dasdfadssda <quddkflty@gmail.com>
2024-02-21 01:16:46 +0900

    폰의 경우 추가

--- src/Service/Format/convertMovesCode.js
+++ src/Service/Format/convertMovesCode.js
@@ -1,14 +1,26 @@
 export const convertMoves = (legalMoves) => {
   return legalMoves.map((move) => {
     // "x"를 포함하는 경우 "x"를 제거합니다.
-    move = move.replace("x", "");
+    if (move.includes("x")) {
+      move = move.replace("x", "");
+    }
 
     // "+" 또는 "#"을 포함하는 경우 처리
     if (move.includes("+") || move.includes("#")) {
       // "+"나 "#"을 제거한 후 반환
-      return move.replace(/[+#]/g, "");
-    } else {
-      return move;
+      move = move.replace(/[+#]/g, "");
     }
+
+    // 폰이 다른 말을 먹었을 경우
+    if (move[0] === move[0].toLowerCase() && move.length === 3) {
+      move = move.slice(-2);
+    }
+
+    // 폰이 끝에 갔을 경우
+    if (move[0] === move[0].toLowerCase() && move.length === 5) {
+      move = move.slice(1);
+    }
+
+    return move;
   });
 };
