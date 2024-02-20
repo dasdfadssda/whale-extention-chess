589b8a79d1516453deeb649d10796a99cdd8e828
dasdfadssda <quddkflty@gmail.com>
2024-02-21 01:18:12 +0900

    가짜 캐슬링 제거

--- src/Service/chessPieceMove/king.js
+++ src/Service/chessPieceMove/king.js
@@ -10,15 +10,6 @@ export const getKingMoves = (x, y, board, color, castlingRights) => {
     [x + 1, y + 1],
   ];
 
-  // 캐슬링 가능한 경우 추가
-  if (color === "white") {
-    if (castlingRights.whiteKingSide) moves.push([7, 6]);
-    if (castlingRights.whiteQueenSide) moves.push([7, 2]);
-  } else {
-    if (castlingRights.blackKingSide) moves.push([0, 6]);
-    if (castlingRights.blackQueenSide) moves.push([0, 2]);
-  }
-
   return moves.filter(([x, y]) => {
     return (
       x >= 0 &&
