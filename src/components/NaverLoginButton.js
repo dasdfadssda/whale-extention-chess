f00e519d3edc057b0f6b947eafef8f1132dbe906
dasdfadssda <quddkflty@gmail.com>
2024-02-22 18:53:59 +0900

    네이버 로그인 로직 리펙터링

--- src/components/NaverLoginButton.js
+++ src/components/NaverLoginButton.js
@@ -1,25 +1,9 @@
 // NaverLoginButton.js
 import React from "react";
 import styled from "styled-components";
-
-// 상태 토큰 생성 함수
-function generateState() {
-  return [...Array(10)].map(() => Math.random().toString(36)[2]).join("");
-}
+import { handleNaverLogin } from "../Service/Auth/NaverLogin";
 
 function NaverLoginButton() {
-  const NAVER_CLIENT_ID = "vVUaQENvhs3KoONKOHpY";
-  const NAVER_CALLBACK_URL = "https://chessextension.web.app/#/callback";
-  // const NAVER_CALLBACK_URL = "http://localhost:3000/#/callback";
-
-  const handleNaverLogin = () => {
-    const state = generateState();
-    const NAVER_LOGIN_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${encodeURIComponent(
-      NAVER_CALLBACK_URL
-    )}&state=${state}`;
-    window.localStorage.setItem("naver_state", state);
-    window.location.href = NAVER_LOGIN_URL;
-  };
 
   return (
     <NaverButton onClick={handleNaverLogin}>
